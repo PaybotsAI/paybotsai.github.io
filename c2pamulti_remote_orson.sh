c7f510fb1e89835e22de8632e372f969604e4dc3
kilroyblockchain <karen@kilroyblockchain.com>
2023-11-19 16:23:19 -0600

    Orson Weems' signed PNG JPG and EPS MF logos

--- c2pamulti_remote_orson.sh
+++ c2pamulti_remote_orson.sh
@@ -8,15 +8,15 @@ filesDirectory="$(pwd)/images"
 manifestFile="$(pwd)/orson_mf.json"
 
 # Output directories for signed images and thumbnails
-outputDirectory="$(pwd)/vip/orson_weems/MF/"
-thumbnailDirectory="$(pwd)/vip/orson_weems/MF/"
+outputDirectory="$(pwd)/vip/orson_weems/MF/EPS"
+thumbnailDirectory="$(pwd)/vip/orson_weems/MF/EPS/thumbnails"
 
 # Base URL for remote c2pa manifest storage
-c2paRemoteBaseURL="https://my.file.baby/vip/orson_weems/MF/"
+c2paRemoteBaseURL="https://my.file.baby/vip/orson_weems/MF/EPS/"
 
 # Create the output and thumbnail directories if they don't exist
-mkdir -p "$outputDirectory"
-# mkdir -p "$thumbnailDirectory"
+# mkdir -p "$outputDirectory"
+mkdir -p "$thumbnailDirectory"
 
 # Loop through each file in the images directory
 for file in "$filesDirectory"/*
@@ -32,14 +32,14 @@ do
 
     echo "Generating thumbnail for: $filename"
     # Generate thumbnail (adjust size as needed, e.g., 100x100 pixels)
-    convert "$file" -resize 100x100 "$thumbnailDirectory/$filename_thumbnail"
+    convert "$file" -resize 100x100 "$thumbnailDirectory/$filename"
 
     echo "Signing: $filename"
     # Construct the absolute path for the output file
     outputFile="$outputDirectory/$filename"
 
     # Run c2patool to sign the file with the manifest
-    $c2paToolPath "$file" -s -r "$c2paRemoteURL" -m "$manifestFile" -o "$outputFile"
+    $c2paToolPath "$file" -s -r "$c2paRemoteURL" -m "$manifestFile" -o "$outputFile" -f
 
     # Check for error in last command
     if [ $? -eq 0 ]; then
