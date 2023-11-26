b4d917a1a1dfd3b6b786a2a38ca246defc4dee0b
kilroyblockchain <karen@kilroyblockchain.com>
2023-11-26 13:07:40 -0600

    updates to Tenant Gallery

--- my-file-baby/src/TenantFileGallery.js
+++ my-file-baby/src/TenantFileGallery.js
@@ -1,26 +1,30 @@
 import React, { useState } from 'react';
-import './TenantFileGallery.css'; // Make sure to have your CSS styles defined as required
+import './TenantFileGallery.css'; // Ensure this CSS file exists in your src directory
 
-async function fetchFilesFromDirectory(apiUrl) {
+const githubToken = process.env.REACT_APP_GITHUB_TOKEN; // Make sure you've added your GitHub token to your .env file
+
+async function fetchFilesFromDirectory(apiUrl, token) {
   let filesList = [];
 
   try {
-    const response = await fetch(apiUrl);
+    const response = await fetch(apiUrl, {
+      headers: {
+        Authorization: `token ${token}` // Add the token to the request headers
+      },
+    });
     const data = await response.json();
 
-    // Log the full data to inspect its structure
-    console.log("Received data:", data);
-
     if (Array.isArray(data)) {
-      // ... existing array handling logic ...
-    } else {
-      // Check if data has a message property, which typically indicates an error response
-      if (data.message) {
-        console.error('GitHub API error:', data.message);
-      } else {
-        // If it's not an error response, handle the single file/directory object
-        console.error('Expected an array, but received an object:', data);
+      for (const item of data) {
+        if (item.type === 'file' && (item.name.endsWith('.jpg') || item.name.endsWith('.png'))) {
+          filesList.push(item);
+        } else if (item.type === 'dir') {
+          const subDirFiles = await fetchFilesFromDirectory(item.url, token);
+          filesList = filesList.concat(subDirFiles);
+        }
       }
+    } else {
+      console.error('GitHub API error:', data.message);
     }
   } catch (error) {
     console.error('Error fetching directory:', error);
@@ -42,40 +46,40 @@ function TenantFileGallery() {
     setError(null);
     if (tenant) {
       const apiUrl = `https://api.github.com/repos/PaybotsAI/paybotsai.github.io/contents/vip/${tenant}`;
-      const fetchedFiles = await fetchFilesFromDirectory(apiUrl);
+      const fetchedFiles = await fetchFilesFromDirectory(apiUrl, githubToken);
       setFiles(fetchedFiles);
     } else {
       setError('Please enter a tenant name.');
     }
   };
 
-  return (
-    <div>
-      <input
-        type="text"
-        value={tenant}
-        onChange={handleTenantChange}
-        placeholder="Enter Tenant Name"
-      />
-      <button onClick={handleSearch}>Load Tenant Files</button>
-      {error && <p className="error-message">{error}</p>}
-      <div className="file-gallery">
-        {files.map((file, index) => (
-          <div key={index} className="file-item">
-            <img
-              src={file.download_url.replace('.jpg', '_thumbnail.png').replace('.png', '_thumbnail.png')}
-              alt={`Thumbnail for ${file.name}`}
-            />
-            <p>{file.name}</p>
-            <a href={file.download_url} target="_blank" rel="noopener noreferrer">
-              View
-            </a>
-          </div>
-        ))}
-      </div>
+  const getThumbnailUrl = (fileUrl) => {
+    return fileUrl.includes('_thumbnail') ? fileUrl : fileUrl.replace('.jpg', '_thumbnail.png').replace('.png', '_thumbnail.png');
+  };
+return (
+  <div>
+    <input
+      type="text"
+      value={tenant}
+      onChange={handleTenantChange}
+      placeholder="Enter Tenant Name"
+    />
+    <button onClick={handleSearch}>Load Tenant Files</button>
+    {error && <p className="error-message">{error}</p>}
+    <div className="file-gallery">
+      {files.map((file, index) => (
+        <div key={index} className="file-item">
+          <img
+            src={getThumbnailUrl(file.download_url)}
+            alt={`Thumbnail for ${file.name}`}
+          />
+          <p>{file.name}</p>
+          <a href={file.download_url} target="_blank" rel="noopener noreferrer">
+            View
+          </a>
+        </div>
+      ))}
     </div>
-  );
-}
-
+  </div>
+);}
 export default TenantFileGallery;
-
