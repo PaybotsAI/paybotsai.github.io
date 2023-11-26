cc2ba863c40aab2c8575470c53121e6b597a6db1
kilroyblockchain <karen@kilroyblockchain.com>
2023-11-26 09:11:39 -0600

    app changes

--- my-file-baby/src/App.js
+++ my-file-baby/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import TenantFileGallery from './TenantFileGallery';
+import logo from './logo.png'; // Make sure the path is correct
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} alt="my.file.baby... MINE!" />
+        <TenantFileGallery />
       </header>
     </div>
   );
 }
 
 export default App;
+
--- my-file-baby/src/TenantFileGallery.js
+++ my-file-baby/src/TenantFileGallery.js
@@ -1,39 +1,75 @@
-import React, { useState, useEffect } from 'react';
-import './TenantFileGallery.css'; // We will create this CSS file in the next step
+import React, { useState } from 'react';
+import './TenantFileGallery.css'; // Make sure to have your CSS styles defined as required
+
+async function fetchFilesFromDirectory(apiUrl) {
+  let filesList = [];
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    // Log the full data to inspect its structure
+    console.log("Received data:", data);
+
+    if (Array.isArray(data)) {
+      // ... existing array handling logic ...
+    } else {
+      // Check if data has a message property, which typically indicates an error response
+      if (data.message) {
+        console.error('GitHub API error:', data.message);
+      } else {
+        // If it's not an error response, handle the single file/directory object
+        console.error('Expected an array, but received an object:', data);
+      }
+    }
+  } catch (error) {
+    console.error('Error fetching directory:', error);
+  }
+
+  return filesList;
+}
 
 function TenantFileGallery() {
   const [tenant, setTenant] = useState('');
   const [files, setFiles] = useState([]);
-
-  useEffect(() => {
-    if (tenant) {
-      const fetchFiles = async () => {
-        try {
-          const response = await fetch(`https://api.github.com/repos/PaybotsAI/paybotsai.github.io/contents/vip/${tenant}`);
-          const data = await response.json();
-          setFiles(data.filter(file => file.name.endsWith('.jpg') || file.name.endsWith('.png')));
-        } catch (error) {
-          console.error('Error fetching files:', error);
-        }
-      };
-
-      fetchFiles();
-    }
-  }, [tenant]);
+  const [error, setError] = useState(null);
 
   const handleTenantChange = (event) => {
     setTenant(event.target.value);
   };
 
+  const handleSearch = async () => {
+    setError(null);
+    if (tenant) {
+      const apiUrl = `https://api.github.com/repos/PaybotsAI/paybotsai.github.io/contents/vip/${tenant}`;
+      const fetchedFiles = await fetchFilesFromDirectory(apiUrl);
+      setFiles(fetchedFiles);
+    } else {
+      setError('Please enter a tenant name.');
+    }
+  };
+
   return (
     <div>
-      <input type="text" value={tenant} onChange={handleTenantChange} placeholder="Enter Tenant Name" />
+      <input
+        type="text"
+        value={tenant}
+        onChange={handleTenantChange}
+        placeholder="Enter Tenant Name"
+      />
+      <button onClick={handleSearch}>Load Tenant Files</button>
+      {error && <p className="error-message">{error}</p>}
       <div className="file-gallery">
-        {files.map(file => (
-          <div key={file.name} className="file-item">
-            <img src={file.download_url.replace('.jpg', '_thumbnail.png').replace('.png', '_thumbnail.png')} alt={`Thumbnail for ${file.name}`} />
+        {files.map((file, index) => (
+          <div key={index} className="file-item">
+            <img
+              src={file.download_url.replace('.jpg', '_thumbnail.png').replace('.png', '_thumbnail.png')}
+              alt={`Thumbnail for ${file.name}`}
+            />
             <p>{file.name}</p>
-            <a href={file.download_url} target="_blank" rel="noopener noreferrer">View</a>
+            <a href={file.download_url} target="_blank" rel="noopener noreferrer">
+              View
+            </a>
           </div>
         ))}
       </div>
